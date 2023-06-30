8b4262768752a187c2e93591a7b7926783dc74ca
Ankush <128672958+uic19bca1130@users.noreply.github.com>
2023-06-30 10:41:25 +0530

    new QR COde Generator

---
+++ src/Component/Qrcode.jsx
@@ -0,0 +1,59 @@
+import { toDataURL } from "qrcode";
+import { useState } from "react";
+import Button from "@mui/material/Button";
+
+function QRCodeGenerator() {
+  const [url, setUrl] = useState("");
+  const [qr, setQr] = useState("");
+
+  const generateQRCode = () => {
+    toDataURL(
+      url,
+     {
+  width: 400,
+  margin: 2,
+  color: {
+    dark: "#335383FF",
+    light: "#EEEEEEFF",
+  },
+},
+
+      (err, dataURL) => {
+        if (err) return console.error(err);
+
+        console.log(dataURL);
+        setQr(dataURL);
+      }
+    );
+  };
+
+  return (
+    <div className="app">
+      <h1>QR Generator😎</h1>
+      <input
+        type="text"
+        placeholder="e.g. https://Ankush.com"
+        value={url}
+        onChange={(e) => setUrl(e.target.value)}
+      />
+      <Button variant="contained" onClick={generateQRCode}>
+        Generate
+      </Button>
+      {qr && (
+        <>
+          <img src={qr} alt="QR Code" />
+          <Button
+            variant="contained"
+            color="success"
+            href={qr}
+            download="qrcode.png"
+          >
+            Download
+          </Button>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default QRCodeGenerator;
